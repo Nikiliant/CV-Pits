84d079c71f14e077284ee2516fb45865a60e6526
Niki Marko <varley193@mail.ru>
2019-04-14 08:30:32 +0300

    Added GUI

--- CV-Klass.py
+++ CV-Klass.py
@@ -1,15 +1,38 @@
 import cv2
+from PyQt5.QtWidgets import QFileDialog, QAction, QMainWindow, QWidget, QApplication
+from PyQt5.QtGui import QWindow
 
 def nothing ():
     pass
-cap = cv2.VideoCapture(0)
+
+file = QFileDialog.getOpenFileName(QWidget, 'Open file', '/home')[0]
+cap = cv2.VideoCapture(file)
 cv2.namedWindow("image")
-R = cv2.createTrackbar("R","image",0,255, nothing)
+R = cv2.createTrackbar("R", "image", 0, 255, nothing)
+G = cv2.createTrackbar("G", "image", 0, 255, nothing)
+B = cv2.createTrackbar("B", "image", 0, 255, nothing)
+
 
 while True:
 
-    if 0xFF==ord("q"):
+    if cv2.waitKey(1) and 0xFF==ord("q"):
         break
+
+    if cv2.waitKey(1) and 0xFF==ord("1"):
+        print("key pressed 1")
+        function(1)
+
+    if cv2.waitKey(1) and 0xFF==ord("2"):
+        print ("key pressed 2")
+        function(2)
+
+    if cv2.waitKey(1) and 0xFF==ord("3"):
+        print('key pressed 3')
+        function(3)
+
+    if cv2.waitKey(1) and 0xFF==ord("4"):
+        print('key pressed 4')
+        function(4)
 
 
 cv2.destroyAllWindows()
--- PR.py
+++ PR.py
@@ -1,42 +1,116 @@
-import cv2
-from PyQt5 import QtWidgets
+import cv2, os, skimage, numpy
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QFileDialog, QMainWindow
-
 class MyWindow (QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        FileDialog = QFileDialog.getOpenFileName(self, 'Open file', '/home')
-        print(FileDialog)
-        self.Cv(FileDialog)
+        self.file = None
+        self.NamesOfTypes = ["1", "2", "3", "4"]
+        self.resize(400, 150)
+        self.gridLayoutWidget = QtWidgets.QWidget(self)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 400, 150))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.label_2.setText("Выходной каталог")
+        self.gridLayout.addWidget(self.label_2, 2, 0, 1, 2)
+        self.lineEdit = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 1, 0, 1, 1)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.gridLayout.addWidget(self.lineEdit_2, 3, 0, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.setText("...")
+        self.pushButton.clicked.connect(lambda: self.setFile())
+        self.gridLayout.addWidget(self.pushButton, 1, 1, 1, 1)
+        self.pushButton_2 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setText("...")
+        self.pushButton_2.clicked.connect(lambda: self.setFolder())
+        self.gridLayout.addWidget(self.pushButton_2, 3, 1, 1, 1)
+        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label.setObjectName("label")
+        self.label.setText("Входной файл")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 2)
+        self.pushButton_3 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setText("Запуск")
+        self.pushButton_3.clicked.connect(lambda: self.Cv(self.lineEdit.text()))
+        self.gridLayout.addWidget(self.pushButton_3, 4, 0, 1, 2)
 
 
-    def ImageFunction1 (self):
+    def ImageFunction (self, Image, Txt):
+        ImgTxt = [Image, Txt]
+        self.file.writelines(str(ImgTxt))
+        self.file.write(str('\r'))
+        return 0
 
-        pass
+    def setFolder (self):
+        self.lineEdit_2.setText(QFileDialog.getExistingDirectory(self, "Выбор каталога", "~/"))
+        return 0
+
+    def setFile(self):
+        self.lineEdit.setText(QFileDialog.getOpenFileName(self, "Открытие файла", "~/")[0])
+        return 0
+
+    def createFile(self):
+        path = self.lineEdit_2.text()
+        filename = self.writeAFile(path)
+        self.file = open(filename, 'a')
+        return 0
+
+    def writeAFile(self, dir):
+        count = 0
+        listoffiles = os.listdir(dir)
+        for a in listoffiles:
+            if ('dataset' in a)==True:
+                count += 1
+
+        if count == 0:
+            fi = 'dataset_1'
+        else:
+            fi = 'dataset_'+str(count+1)
+
+
+        return fi
 
     def Cv (self, f):
+        self.createFile()
         if f[0] != "":
             cap = cv2.VideoCapture(f[0])
+            img = cap.read()
             cv2.namedWindow("image")
             while True:
-                if cv2.waitKey(1) and 0xFF == ord("q"):
+                k = chr(cv2.waitKey())
+                if k=='q':
                     break
 
-                if cv2.waitKey(1) and 0xFF == ord("1"):
+                if k=='1':
+                    MyWindow.ImageFunction(self, img, self.NamesOfTypes[0])
                     print("key pressed 1")
 
-                if cv2.waitKey(1) and 0xFF == ord("2"):
+                if k=='2':
+                    MyWindow.ImageFunction(self, img, self.NamesOfTypes[1])
                     print("key pressed 2")
 
-                if cv2.waitKey(1) and 0xFF == ord("3"):
+                if k=='3':
+                    MyWindow.ImageFunction(self, img, self.NamesOfTypes[2])
                     print('key pressed 3')
 
-                if cv2.waitKey(1) and 0xFF == ord("4"):
+                if k=='4':
+                    MyWindow.ImageFunction(self, img, self.NamesOfTypes[3])
                     print('key pressed 4')
             cv2.destroyAllWindows()
+            self.file.close()
+
 
 if __name__=="__main__":
     import sys
